eb90f5e2651078b813487905dec0862fb132f83f
Сергей Трофимов <sm.trofimov2004@gmail.com>
2022-10-19 22:09:58 +0300

    exponent

--- Matrix/Matrix/Program.cs
+++ Matrix/Matrix/Program.cs
@@ -42,20 +42,25 @@ namespace test
         static int[,] DoExponentiation(int[,] matr, int count)
         {
             var variable = 0;
-            int[,] exponentedMatr = new int[matr.GetLength(0), matr.GetLength(1)];
-            if (count == 1)
-                return matr;
+            int[,] exponentedMatr = matr;
             if (exponentedMatr.GetLength(0) == exponentedMatr.GetLength(1))
                 for (int i = 0; i < count - 1; i++)
-                    for (int j = 0; j < exponentedMatr.GetLength(0); j++)
-                        for (int k = 0; k < exponentedMatr.GetLength(0); k++)
-                        {
-                            for (int l = 0; l < exponentedMatr.GetLength(0); l++)
-                                variable += matr[j, l] * matr[l, k];
-                            exponentedMatr[j, k] = variable;
-                            variable = 0;
-                        }
+                    exponentedMatr = DoMultiplication(exponentedMatr);
             return exponentedMatr;
+        }
+        static int[,] DoMultiplication(int[,] matr)
+        {
+            var variable = 0;
+            int[,] newMatr = new int[matr.GetLength(0), matr.GetLength(1)];
+            for (int j = 0; j < newMatr.GetLength(0); j++)
+                for (int k = 0; k < newMatr.GetLength(0); k++)
+                {
+                    for (int l = 0; l < newMatr.GetLength(0); l++)
+                        variable += matr[j, l] * matr[l, k];
+                    newMatr[j, k] = variable;
+                    variable = 0;
+                }
+            return newMatr;
         }
     }
 }
