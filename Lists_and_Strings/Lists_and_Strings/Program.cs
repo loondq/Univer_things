8e0a5675d1debcc22d4abfa658199008ffb8d849
Сергей Трофимов <sm.trofimov2004@gmail.com>
2022-10-19 12:49:24 +0300

    1 and 2 tasks

---
+++ Lists_and_Strings/Lists_and_Strings/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace test
+{
+    class Project
+    {
+        static void Main()
+        {
+            String str = "Шалаш комок Два три абвгд катер мадам";
+            Console.WriteLine(FindPalindromes(str));
+            Console.WriteLine(IsFirstLetterOccursAgain(str));
+        }
+        static string FindPalindromes(string str)
+        {
+            var parts = str.ToLower().Split(" ");
+            var palindromes = "";
+            var fl = true;
+            for (int k = 0; k < parts.Length; k++)
+            {
+                for (int i = 0; i < parts[k].Length / 2; i++)
+                {
+                    fl = true;
+                    if (parts[k][i] != parts[k][parts[k].Length - (i + 1)])
+                        fl = false;
+                }
+                if (fl)
+                    palindromes += $"{parts[k]} ";
+            }
+            return palindromes;
+        }
+        static bool IsFirstLetterOccursAgain(string str)
+        {
+            str = str.ToLower();
+            for (int i = 1; i < str.Length; i++)
+                if (str[0] == str[i])
+                    return true;
+            return false;
+        }
+    }
+}
