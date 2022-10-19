cea1c2fec369363856c323a7a8a57f8a589e7254
Сергей Трофимов <sm.trofimov2004@gmail.com>
2022-10-19 12:57:40 +0300

    ulearn task

---
+++ Distance from point to segment/Distance from point to segment/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace DistanceTask
+{
+    public static class GetDistance
+    {
+        static void Main()
+        {
+            Console.WriteLine(GetDistanceToSegment(-1, 2, -1, 2, 1, -2));
+        }
+        public static double GetDistanceToSegment(double ax, double ay, double bx, double by, double x, double y)
+        {
+            double triangleArea = 0.5 * ((ax - x) * (by - y) - (bx - x) * (ay - y));
+            double distanceFromAToB = Math.Sqrt((bx - ax) * (bx - ax) + (by - ay) * (by - ay));
+            double distance = Math.Abs((triangleArea / distanceFromAToB) * 2);
+            double distanceFromAToC = Math.Sqrt((x - ax) * (x - ax) + (y - ay) * (y - ay));
+            double distanceFromBToC = Math.Sqrt((x - bx) * (x - bx) + (y - by) * (y - by));
+            if (distanceFromAToC * distanceFromAToC > distanceFromBToC * distanceFromBToC + distanceFromAToB * distanceFromAToB)
+                return distanceFromBToC;
+            if (distanceFromBToC * distanceFromBToC > distanceFromAToC * distanceFromAToC + distanceFromAToB * distanceFromAToB)
+                return distanceFromAToC;
+            if (triangleArea == 0)
+            {
+                if ((x - ax) * (by - ay) - (bx - ax) * (y - ay) == 0 && (distanceFromAToB != 0))
+                    return 0;
+                else
+                    return Math.Min(distanceFromAToC, distanceFromBToC);
+            }
+            return distance;
+        }
+
+
+    }
+}
