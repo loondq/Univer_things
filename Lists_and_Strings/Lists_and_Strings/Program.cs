caf6cf5d96d5a8c329a8dfa9c0a853ca89709906
Сергей Трофимов <sm.trofimov2004@gmail.com>
2022-10-23 18:49:59 +0300

    task 1 - 5

--- Lists_and_Strings/Lists_and_Strings/Program.cs
+++ Lists_and_Strings/Lists_and_Strings/Program.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 
-namespace test
+namespace ListsAndStrings
 {
     class Project
     {
         static void Main()
         {
-            String str = "Шалаш комок Два три абвгд катер мадам";
+            String str = "Шалаш комок Два три абвгд катер абвгд три мадам кукуруза";
+            string str2 = "кот собака кот кот кот";
             Console.WriteLine(FindPalindromes(str));
             Console.WriteLine(IsFirstLetterOccursAgain(str));
+            Console.WriteLine(IsOrderedString(str));
+            Console.WriteLine($"самое длинное - {FindLongestAndShortestWords(str)[0]}, самое короткое - {FindLongestAndShortestWords(str)[1]}");
+            Console.WriteLine(RemoveRepeatebleWords(str));  
         }
         // 1 task
         static string FindPalindromes(string str)
@@ -37,6 +41,50 @@ namespace test
                 if (str[0] == str[i])
                     return true;
             return false;
+        }
+        static bool IsOrderedString(string str)
+        {
+            bool fl = true;
+            for (int i = 0; i < str.Length - 1; i++)
+                if (str[i] > str[i + 1])
+                    fl = false;
+            return fl;
+        }
+        /// <returns>1. Longest | 2. Shortest</returns>
+        static string[] FindLongestAndShortestWords(string str)
+        {
+            string[] longestAndShortestWords = new string[2]; 
+            var parts = str.ToLower().Split();
+            int longest = int.MinValue, shortest = int.MaxValue;
+            for (int i = 0; i < parts.Length; i++)
+            {
+                if (parts[i].Length < shortest)
+                {
+                    longestAndShortestWords[1] = parts[i];
+                    shortest = parts[i].Length;
+                }
+                if (parts[i].Length > longest)
+                {
+                    longestAndShortestWords[0] = parts[i];
+                    longest = parts[i].Length;
+                }
+            }
+            return longestAndShortestWords;
+        }
+        static string RemoveRepeatebleWords(string str)
+        {
+            var parts = str.ToLower().Split();
+            for (int i = 0; i < parts.Length - 1; i++)
+            {
+                for (int y = i; y < parts.Length - 1; y++)
+                {
+                    if (parts[i] == parts[y + 1])
+                        parts[y + 1] = "";
+                }
+            }
+            string result = String.Join(" ", parts);
+            while (result.Contains("  ")) { result = result.Replace("  ", " "); }
+            return result;
         }
     }
 }
