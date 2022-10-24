69c80b49e95b7ac3b9a101bfe1e2e1ff94e92a2d
Сергей Трофимов <sm.trofimov2004@gmail.com>
2022-10-24 15:04:10 +0300

    text working

--- Functions/Functions/Program.cs
+++ Functions/Functions/Program.cs
@@ -6,7 +6,13 @@ namespace Functions
     {
         static void Main()
         {
-
+            StreamWriter writer = new StreamWriter("D:\\texts\\output.txt");
+            for (int i = 1; i <= 10; i++) 
+                writer.WriteLine($"Text {i}");
+            writer.Close();
+            StreamReader reader = new StreamReader("D:\\texts\\output.txt");
+            string text = reader.ReadToEnd();
+            Console.WriteLine(text);
         }
     }
 }
