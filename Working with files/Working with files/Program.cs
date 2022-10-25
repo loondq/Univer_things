6f1622465ae2d35ddd194123a3c92ece2f62b778
Сергей Трофимов <sm.trofimov2004@gmail.com>
2022-10-25 13:07:51 +0300

    25.10.2022

--- Working with files/Working with files/Program.cs
+++ Working with files/Working with files/Program.cs
@@ -6,7 +6,18 @@ namespace Files
     {
         static void Main()
         {
-
+            StreamReader reader = new StreamReader("D:\\texts\\test.txt");
+            var parts = reader.ReadToEnd().Split();
+            Dictionary<string, int> words = new Dictionary<string, int>();
+            foreach (var word in parts)
+            {
+                if (!words.ContainsKey(word))
+                    words.Add(word, 1);
+                else
+                    words[word]++;
+            }
+            foreach (var word in words)
+                Console.WriteLine($"{word.Key} - {word.Value}");
         }
     }
 }
