f55ced98693ef2423e6ef98e450a36f2a9c8cade
Сергей Трофимов <sm.trofimov2004@gmail.com>
2022-11-14 16:55:35 +0300

    14.11.2022

---
+++ ООП/ООП/Fraction.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    public class Fraction
+    {
+        private double numerator;
+        private double denominator;
+
+        public void SetNum(int a)
+        {
+            numerator = a;
+        }
+        public void SetDenom(int b)
+        {
+            if (b == 0)
+                throw new ArgumentException("Нельзя делить на ноль");
+            denominator = b;
+        }
+        public double GetNum()
+        {
+            return numerator;
+        }
+        public double GetDenum()
+        {
+            return denominator; 
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"{numerator/denominator}");
+        }
+    }
+}
--- ООП/ООП/Program.cs
+++ ООП/ООП/Program.cs
@@ -6,6 +6,11 @@ namespace OOP
     {
         static void Main()
         {
+            Fraction f = new Fraction();
+            f.SetNum(11);
+            f.SetDenom(12);
+            f.Print();
+            Console.WriteLine($"Fraction - {f.GetNum()} / {f.GetDenum()}");
 
         }
     }
